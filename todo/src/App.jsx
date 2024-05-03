885a90baef03fd8a4e751add6066c2fbc246046d
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-05-03 22:55:52 +0200

    Created a calendar template

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import Calendar from './components/Calendar/Calendar'
+import Todos from './components/Todos/Todos'
+import AddTodo from './components/AddTodo/AddTodo'
+import Totals from './components/Totals/Totals'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <Calendar/>
+      <Todos/>
+      <AddTodo/>
+      <Totals/>
+    </div>
   )
 }
 
--- todo/src/components/Calendar/Calendar.jsx
+++ todo/src/components/Calendar/Calendar.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import "./Calendar.css";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Calendar() {
   return (
-    <div>Calendar</div>
-  )
+    <div className="calendar">
+      <div className="calendar-header">
+        <div className="month">
+          <span className="month-name">May</span>
+          <span className="year">2024</span>
+        </div>
+        <div className="cal-btns">
+          <button className="prev-btn">
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn">
+            <FaChevronRight />
+          </button> 
+        </div>
+    </div>
+    <div className="weekdays">
+          <div>Sun</div>
+          <div>Mon</div>
+          <div>Tue</div>
+          <div>Wed</div>
+          <div>Thu</div>
+          <div>Fri</div>
+          <div>Sat</div>
+        </div>
+        <div className="days">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>10</div>
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+          <div>14</div>
+          <div>15</div>
+          <div>16</div>
+          <div>17</div>
+          <div>18</div>
+          <div>19</div>
+          <div>20</div>
+          <div>21</div>
+          <div>22</div>
+          <div>23</div>
+          <div>24</div>
+          <div>25</div>
+          <div>26</div>
+          <div>27</div>
+          <div>28</div>
+          <div>29</div>
+          <div>30</div>
+          <div>31</div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Calendar
+
+export default Calendar;
