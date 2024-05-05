b49fc03fe125161eb7f03a428b82308a816160c0
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-05-05 20:53:11 +0200

    created todo UI

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Todo.css'
+import { FaCheck } from "react-icons/fa";
 
 function Todo() {
+  const [checked, setChecked] = useState(false);
+
+
+  const handleChecked = () => {
+    setChecked(!checked);
+  }
+
+
   return (
-    <div>Todo</div>
+    <div className='todo'>
+      <div className="left">
+        <h3 className='task-header'>Complete Project</h3>
+        <p className='task-desc'>
+          Finish implementing the task Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, iure!
+        </p>
+      </div>
+      <div className="right">
+        <label htmlFor="done" className="check" >
+          <input type="checkbox" name="done" id="done" />
+          <span className={checked=== true ? 'radio checked' : 'radio'} onClick={handleChecked}>{checked===true ? <FaCheck className='icon'/> : ""}</span>
+        </label>
+      </div>
+    </div>
   )
 }
 
