1cfcabe570f3f2d7118f3e84feba880575795b53
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-05-06 19:17:53 +0200

    added action buttons on todo component

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,32 +1,46 @@
-import React, { useState } from 'react'
-import './Todo.css'
+import React, { useState } from "react";
+import "./Todo.css";
 import { FaCheck } from "react-icons/fa";
+import { MdEdit, MdDeleteForever } from "react-icons/md";
 
 function Todo() {
   const [checked, setChecked] = useState(false);
 
-
   const handleChecked = () => {
     setChecked(!checked);
-  }
-
+  };
 
   return (
-    <div className='todo'>
+    <div className="todo">
       <div className="left">
-        <h3 className='task-header'>Complete Project</h3>
-        <p className='task-desc'>
-          Finish implementing the task Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, iure!
-        </p>
+        <label htmlFor="done" className="check">
+          <input type="checkbox" name="done" id="done" />
+          <span
+            className={checked === true ? "radio checked" : "radio"}
+            onClick={handleChecked}>
+            {checked === true ? <FaCheck className="icon" /> : ""}
+          </span>
+        </label>
+        <div className="content">
+          <h3 className={checked === true ? "task-done" : ""}>Complete Project</h3>
+          <p className={checked === true ? "task-done" : ""}>
+            Finish implementing the task Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Facere, iure!
+          </p>
+        </div>
       </div>
       <div className="right">
-        <label htmlFor="done" className="check" >
-          <input type="checkbox" name="done" id="done" />
-          <span className={checked=== true ? 'radio checked' : 'radio'} onClick={handleChecked}>{checked===true ? <FaCheck className='icon'/> : ""}</span>
-        </label>
+        <div className="action-btns">
+          <button className="edit-btn">
+            <MdEdit />
+          </button>
+          <button className="delete-btn">
+            <MdDeleteForever />
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
