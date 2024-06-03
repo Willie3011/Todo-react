bb465d4a9c863578594b58b41e677a69da4370c6
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-06-03 15:07:18 +0200

    Removed sensitive data from the project

--- todo/src/config/firestore.js
+++ todo/src/config/firestore.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBl39A9Vkia8cTcJNYfBR9Oc2OSS2xUkms",
-  authDomain: "react-todo-1b219.firebaseapp.com",
-  projectId: "react-todo-1b219",
-  storageBucket: "react-todo-1b219.appspot.com",
-  messagingSenderId: "425556678244",
-  appId: "1:425556678244:web:5acd3d879b810301db7171"
+  apiKey: import.meta.env.APP_API_KEY,
+  authDomain: import.meta.env.APP_AUTH_DOMAIN,
+  projectId: import.meta.env.APP_PROJECT_ID,
+  storageBucket: import.meta.env.APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.APP_MESSAGE_SENDER_ID,
+  appId: import.meta.env.APP_APP_ID
 };
 
 // Initialize Firebase
